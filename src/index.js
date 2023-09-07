dfbacce14c01da7de687920507125f5f7ee3fa52
Kirill <70847294+darkwindyclover@users.noreply.github.com>
2023-09-07 13:14:14 +0300

    auto n choose

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ alert("Let's integrate!");
 p1 = prompt("First point", 0);
 p2 = prompt("Second point", 0);
 function calculate(a, b, integFunc) {
-    n = 5           // Subintervals amount 
-    dX = (b-a)/n    // Delta X
+    n = 10000  
+    prevSum = 0
     sum = 0
-    for (let i = 1; i<=n; i++) {
-        sum += integFunc(a+i*dX);
+    for (let j = 16; j<n; j*=2) {
+        sum = 0
+        dX = (b-a)/j    // Delta X      
+        for (let i = 1; i<=j; i++) {
+            sum += integFunc(a+i*dX);
+        }
+        sum = sum*dX
+        console.log("prevsum = " + prevSum + ", sum = " + sum + ", j = " + j)
+        if (Math.abs(prevSum-sum) < 0.01) { break }
+        prevSum = sum
     }
-    sum = sum*dX
+    
     alert("Ready:" + sum);
 }
 
