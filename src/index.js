9f71a1e463c2f08400de098c002cd21b10323631
Kirill <70847294+darkwindyclover@users.noreply.github.com>
2023-09-07 12:44:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert("Let's integrate!")
+alert("Let's integrate!");
+p1 = prompt("First point", 0);
+p2 = prompt("Second point", 0);
+function calculate(a, b, integFunc) {
+    n = 5           // Subintervals amount 
+    dX = (b-a)/n    // Delta X
+    sum = 0
+    for (let i = 1; i<=n; i++) {
+        sum += integFunc(a+i*dX);
+    }
+    sum = sum*dX
+    alert("Ready:" + sum);
+}
+
+function Func3(x) {
+    return x*x-x+1
+}
+
+calculate(p1, p2, Func3)
